20d8cbeff3f5c7041b81c6a1645e747bf35a3132
htet_lin_maung <htet.lin.maung.@fusionsol.com>
2019-12-16 17:29:23 +0630

    update

--- routes/crafting_components.js
+++ routes/crafting_components.js
@@ -1,55 +1,60 @@
-const express = require('express')
-const Crafting_components = require('../models/Crafting_components')
-const router = express.Router()
+const express = require('express');
+const Crafting_components = require('../models/Crafting_components');
+const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const crafting_components = await Crafting_components.find()
-    res.json(crafting_components)
+    const crafting_components = await Crafting_components.find();
+    res.json(crafting_components);
   } catch (err) {
-    res.status(500).json({ msg: err })
+    res.status(500).json({ msg: err });
   }
-})
+});
 
 router.get('/:id', async (req, res) => {
   try {
-    const crafting_component = await Crafting_components.findById(req.params.id)
-    res.json(crafting_component)
+    const crafting_component = await Crafting_components.findById(
+      req.params.id
+    );
+    res.json(crafting_component);
   } catch (err) {
-    res.status(500).json({ msg: err })
+    res.status(500).json({ msg: err });
   }
-})
+});
 
 router.post('/', async (req, res) => {
   try {
     for (const [key, value] of Object.entries(req.body)) {
+      
       if (!value)
-        return res.status(400).json({ msg: 'Fields must not be null!' })
+        return res.status(400).json({ msg: 'Fields must not be null!' });
     }
 
-    const { name, weight, rarity, baseID } = req.body
+    const { name, weight, rarity, baseID } = req.body;
 
     const component = new Crafting_components({
       name,
       weight,
       rarity,
       baseID
-    })
+    });
 
-    const response = await component.save()
-    res.status(200).json(response)
+    const response = await component.save();
+    res.status(200).json(response);
   } catch (err) {
-    res.status(500).json({ msg: err })
+    res.status(500).json({ msg: err });
   }
-})
+});
 
 router.delete('/:id', async (req, res) => {
   try {
-    const response = await Crafting_components.deleteOne({ _id: req.params.id })
-    res.status(401).json(response)
+    const response = await Crafting_components.deleteOne({
+      _id: req.params.id
+    });
+    res.status(401).json(response);
   } catch (err) {
-    res.status(500).json({ msg: err })
+    res.status(500).json({ msg: err });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
--- routes/junks.js
+++ routes/junks.js
@@ -1,57 +1,63 @@
-const express = require('express')
-const Junks = require('../models/Junks')
-const CraftingJunk = require('../models/Crafting_Junk')
+const express = require('express');
+const Junks = require('../models/Junks');
+const CraftingJunk = require('../models/Crafting_Junk');
+const Crafting_components = require('../models/Crafting_components');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const junks = await Junks.find()
-    res.json(junks)
+    const junks = await Junks.find();
+    res.json(junks);
   } catch (err) {
-    res.status(500).json({ msg: err })
+    res.status(500).json({ msg: err });
   }
-})
+});
 
 router.get('/:id', async (req, res) => {
   try {
-    const junk = await Junks.findById(req.params.id)
-    res.json(junk)
+    const junk = await Junks.findById(req.params.id);
+    res.json(junk);
   } catch (err) {
-    res.status(500).json({ msg: err })
+    res.status(500).json({ msg: err });
   }
-})
+});
 
 router.post('/', async (req, res) => {
   try {
     for (const [key, value] of Object.entries(req.body)) {
+      if (key == 'weight' && value == 0) {
+        continue;
+      }
       if (!value)
-        return res.status(400).json({ msg: 'Fields must not be null!' })
+        return res.status(400).json({ msg: 'Fields must not be null!' });
     }
 
-    const { name, weight, baseID, components } = req.body
+    const { name, weight, baseID, components } = req.body;
 
     const junk = new Junks({
       name,
       weight,
       baseID
-    })
+    });
 
-    const junk_res = await junk.save()
-    let component = null
-    for (const [component_id, quantity] of Object.entries(components)) {
+    const junk_res = await junk.save();
+    let component = null;
+    const crafting_components = await Crafting_components.find();
+    for (const [component_name, quantity] of Object.entries(components)) {
       component = new CraftingJunk({
         junk_id: junk_res._id,
-        component_id,
+        component_id: crafting_components.find(v => v.name == component_name)
+          ._id,
         quantity
-      })
-      const transition_res = await component.save()
+      });
+      const transition_res = await component.save();
     }
 
-    res.status(200).json(junk)
+    res.status(200).json(junk);
   } catch (err) {
-    res.status(500).json({ msg: err })
+    res.status(500).json({ msg: err });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
